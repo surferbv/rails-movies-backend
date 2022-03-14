2b5672872372fd571808e78a5519429508939b27
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2022-03-14 12:49:11 -0700

    Init

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
+  # root                  to: 'movies#index'
+  get   '/movies',          to: 'movies#index'
+  get   '/movies/new',      to: 'movies#new'
+  post  '/movies',          to: 'movies#create'
+  get   '/movies/:id',      to: 'movies#show'
+  get   '/movies/:id/edit', to: 'movies#edit'
+  put   '/movies/:id',      to: 'movies#update'
+  delete'/movies/:id',      to: 'movies#destroy'
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Movie.create(name: "Titanic", rating: 5)
+Movie.create(name: "We were soldiers", rating: 4)
+Movie.create(name: "L’amour quand elle nous tient", rating: 5)
+Movie.create(name: "Nobody's Fool", rating: 2)
