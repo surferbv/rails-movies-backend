28838b1b197f0546cd02a106a3b407925df743fe
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2022-03-15 15:16:53 -0700

    Remove duplicate.

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Movies", type: :request do
 
-  context "Request test" do
+  context "request test" do
 
     it "GET /index"
     it "POST /create"
-    it "GET /show"
     it "GET /show"
     it "PUT /update"
     it "DELETE /destroy"
