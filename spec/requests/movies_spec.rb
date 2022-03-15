148495aa1285355812e7d4728f69fc096c1fb6d1
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2022-03-15 15:11:26 -0700

    After using rspec g rspec:controller movies

---
+++ spec/requests/movies_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Movies", type: :request do
+
+  context "Request test" do
+
+    it "GET /index"
+    it "POST /create"
+    it "GET /show"
+    it "GET /show"
+    it "PUT /update"
+    it "DELETE /destroy"
+
+  end
+
+end
