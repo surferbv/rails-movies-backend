faa5a8840738cfabebc0cc9c5af9188ee6993197
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2022-03-15 15:04:55 -0700

    These routes are only used for rails action view.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   get 'errors/internal_server_error'
   # root                  to: 'movies#index'
   get   '/movies',          to: 'movies#index'
-  get   '/movies/new',      to: 'movies#new'
+  # get   '/movies/new',      to: 'movies#new'
   post  '/movies',          to: 'movies#create'
   get   '/movies/:id',      to: 'movies#show'
-  get   '/movies/:id/edit', to: 'movies#edit'
+  # get   '/movies/:id/edit', to: 'movies#edit'
   put   '/movies/:id',      to: 'movies#update'
   delete'/movies/:id',      to: 'movies#destroy'
 
