27270868410d2d392418d2e801a2c20e57520011
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2022-03-15 14:07:05 -0700

    Initial controller crud actions created.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,36 +1,60 @@
 class MoviesController < ApplicationController
   def index
-    movies = Movie.all
-    render json: movies
-end
-
-def new
-  render status: 501
-end
-
-def create
-  render status: 501
-end
-
-def show
-  movie = Movie.find(params[:id])
-  render json: movie
-end
-
-def edit
-  render status: :not_implemented
-end
-
-def update
-  render status: 501
-end
-
-def destroy
-  render status: 501
-end
-
-private
-  def movie_params
-    params.require(:movie).permit(:name, :rating)
+      movies = Movie.all
+      render json: movies
   end
+  
+  # get
+  # responsible for rendering the view before create
+  def new
+    render status: :not_implemented 
+  end
+
+  # post
+  # format { "name": "acme", "raiting": "10" }
+  def create
+    # .create! is equivalent to .new followed by .save! (throws an error if saving fails). It's also just a wee bit shorter
+    Movie.create!(params[:movie])
+  end
+
+  # get
+  def show
+    movie = Movie.find(params[:id])
+    render json: movie
+  end
+
+  # get
+  # responsible for rendering the view before update
+  def edit
+    render status: :not_implemented
+  end
+
+  # put
+  # responsible for updating the existing record
+  # format { "name": "acme", "raiting": "10" }
+  def update
+    movie = Movie.find(params[:id])
+    if movie
+      movie.update(movie_params)
+      render json: {message: "Movie successfully updated."}, status: 200
+    else
+      render json: {error: "Movie was not updated."}, status: 400
+    end
+  end
+
+  # delete
+  def destroy
+    movie = Movie.find(params[:id])
+    if movie
+      movie.destroy()
+      render json: {message: "Movie has been successfully deleted."}, status: 200
+    else
+      render json: {error: "Movie was not deleted."}, status: 400
+    end
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:name, :rating)
+    end
 end
