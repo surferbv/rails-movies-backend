c50d74ae505576e3179e75082b836214d2e2ccf5
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2022-03-18 16:47:16 -0700

    Checking count.

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -14,20 +14,32 @@ RSpec.describe MoviesController, type: :request do
     it "GET /index expect 200" do
       get "/movies"
       expect(response).to have_http_status(:success)
+      
+      # check number of records 
+      expect(Movie.all.count).to eq(1)
     end
     
     it "POST /create" do
       headers = { "CONTENT_TYPE" => "application/json" }
       post "/movies", params: @movie_json
       expect(response).to have_http_status(:created)
+
+      # check number of records 
+      expect(Movie.all.count).to eq(2)
+
+      # test bad formed json
     end
 
     it "GET /show" do
       get "/movies/#{@movie.id}"
       expect(response).to have_http_status(:success)
+
+      # check number of records 
+      expect(Movie.all.count).to eq(1)
+
+      # test what if id does not exists
     end
     
-    # TODO: Not working
     it "PUT /update" do
       put "/movies/#{@movie.id}", params: {
         movie:{
@@ -36,11 +48,23 @@ RSpec.describe MoviesController, type: :request do
         }
       }
       expect(response).to have_http_status(:ok)
+
+      # check that response body has reflected changes
+      sym_json = response_body.deep_symbolize_keys
+      print_it(sym_json)
+      expect(sym_json[:name]).to eq(@movie.name)
+      expect(sym_json[:rating]).to eq(6)
+
+      # check number of records 
+      expect(Movie.all.count).to eq(1)
     end
     
     it "DELETE /destroy" do
       delete "/movies/#{@movie.id}"
       expect(response).to have_http_status(:success)
+
+      # check number of records 
+      expect(Movie.all.count).to eq(0)
     end
 
 
@@ -48,6 +72,10 @@ RSpec.describe MoviesController, type: :request do
 
   def response_body()
     JSON.parse(response.body)
+  end
+
+  def print_it(response)
+    puts response
   end
 
 end
