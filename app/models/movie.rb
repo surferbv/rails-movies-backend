afdc1ed00794261d3efdf4898d4b8c10891d136c
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2022-03-20 13:19:00 -0700

    validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ApplicationRecord
-    
+    validates :name, presence: true, length: { maximum: 40 } 
+    validates :rating, presence: true, numericality: { only_integer: true, in: 1..11 }
 end
